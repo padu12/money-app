d2a0577c04c0f9cfa548bcda8594939b01deeef1
Bohdan <notpadumink01@gmail.com>
2023-04-26 08:14:41 +0300

    first commit

---
+++ src/main/java/ua/odessa/moneyApp/MoneyAppApplication.java
@@ -0,0 +1,13 @@
+package ua.odessa.moneyApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/odessa/moneyApp/MoneyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.odessa.moneyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
